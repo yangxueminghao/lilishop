0baa050c860a76194152b3e7b245cad459221a5f
Your Name <you@example.com>
2022-01-06 15:07:29 +0800

    修改

---
+++ xxl-job/gitlab.sh
@@ -0,0 +1,4 @@
+echo '开始启动'
+docker run -d -p 443:443 -p 80:80 -p 222:22 --name gitlab -v /home/gitlab/config:/etc/gitlab -v /home/gitlab/logs:/var/log/gitlab -v /home/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce
+echo '启动完成'
+
