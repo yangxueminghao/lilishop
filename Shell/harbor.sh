e89d98e05259c54bdb00eee7432ffc82feee2ac1
Your Name <you@example.com>
2022-01-06 18:19:49 +0800

    修改部署組件

--- Shell/harbor.sh
+++ Shell/harbor.sh
@@ -41,12 +41,64 @@
 #镜像id  [grep -w 全量匹配镜像名] [awk 获取信息行的第三列，即镜像ID]
 # IID=$(docker images | grep -w "$SERVER_NAME" | awk '{print $3}')
 
+# 生成CA证书私钥
+# openssl genrsa -out ca.key 4096
+# 生成CA证书
+# openssl req -sha512 -new \
+#     -subj "/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=192.168.159.132" \
+#     -key 192.168.159.132.key \
+#     -out 192.168.159.132.csr
+# 生成服务器私钥
+# openssl genrsa -out 192.168.159.132.key 4096
+# 生成服务端CA证书
+# openssl req -x509 -new -nodes -sha512 -days 3650 \
+#  -subj "/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=192.168.159.132" \
+#  -key ca.key \
+#  -out ca.crt
+# 生成x509 v3 扩展文件
+# cat > v3.ext <<-EOF
+# authorityKeyIdentifier=keyid,issuer
+# basicConstraints=CA:FALSE
+# keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+# extendedKeyUsage = serverAuth
+# subjectAltName = IP:192.168.159.132
+# EOF
+# 生成主机证书
+# openssl x509 -req -sha512 -days 3650 \
+#     -extfile v3.ext \
+#     -CA ca.crt -CAkey ca.key -CAcreateserial \
+#     -in 192.168.159.132.csr \
+#     -out 192.168.159.132.crt
+# 复制服务端证书文件到认证目录
+# mkdir -p /data/cert/
+# cp 192.168.159.132.crt /data/cert/
+# cp 192.168.159.132.key /data/cert/
+# 增加docker证书可信
+# openssl x509 -inform PEM -in 192.168.159.132.crt -out 192.168.159.132.cert
+# cp 192.168.159.132.cert /etc/docker/certs.d/192.168.159.132/
+# cp 192.168.159.132.key /etc/docker/certs.d/192.168.159.132/
+# cp ca.crt /etc/docker/certs.d/192.168.159.132/
+
 echo '开始启动harbor'
+cd /home
+mkdir harbor-keys
+cd harbor-keys
+openssl genrsa -out rsa_private.key 2048
+# 这一步需要填一些东西，记得别填密码，另外 Common Name 填域名或者host里对应当前ip的值(如：master)
+openssl req -new -key rsa_private.key -out rsa_private.csr
+openssl x509 -req -days 365 -in rsa_private.csr -signkey rsa_private.key -out rsa_private.crt
+cd ..
 wget https://github.com/goharbor/harbor/releases/download/v2.1.0/harbor-offline-installer-v2.1.0.tgz
 tar zxvf harbor-offline-installer-v2.1.0.tgz
 mkdir harbor
 cd harbor/
 cp harbor.yml.tmpl harbor.yml
+vim ./harbor/harbor.yml
+    hostname: 192.168.0.28
+    https:
+      port: 443
+      certificate: /home/harbor-keys/rsa_private.crt
+      private_key: /home/harbor-keys/rsa_private.key
 bash ./prepare
 bash ./install.sh
 echo '启动完成'
