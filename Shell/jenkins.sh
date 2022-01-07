009772c100c475a369e4298b571ca755e0222231
Your Name <you@example.com>
2022-01-07 16:49:56 +0800

    修改部署組件

--- Shell/jenkins.sh
+++ Shell/jenkins.sh
@@ -6,6 +6,15 @@ echo '开始启动jenkins'
 
 #镜像id  [grep -w 全量匹配镜像名] [awk 获取信息行的第三列，即镜像ID]
 #IID=$(docker images | grep -w "$SERVER_NAME" | awk '{print $3}') openauth:${BUILD_NUMBER}
+#1 Build / Publish Docker Image
+#1
+# Directory for Dockerfile .
+# Cloud dockerserver
+# image zcfx/openauth:${BUILD_NUMBER}
+# push zcfx/openauth:${BUILD_NUMBER} dockerhub username password
+#2 Action to choose zcfx/openauth:${BUILD_NUMBER}
+#2 Start/Stop Docker Containers
+#2 Action to choose zcfx/openauth:${BUILD_NUMBER}
 mkdir /home/jenkins_home
 chmod 777 -R /home/jenkins_home
 docker run -d -p 8080:8080 -p 50000:50000 -v /home/jenkins_home:/var/jenkins_home jenkins/jenkins
