126807b61f474b1548508a217481c7ef40450e38
Your Name <you@example.com>
2022-01-12 16:36:48 +0800

    修改部署組件

--- Shell/minio.sh
+++ Shell/minio.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 echo '开始启动minio'
-docker run -p 9000:9000 \
+docker run -p 9000:9000 -p 9001:9001 \
   -e "MINIO_ROOT_USER=AKIAIOSFODNN7EXAMPLE" \
   -e "MINIO_ROOT_PASSWORD=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
-  minio/minio server /data
+  minio/minio server /data --console-address ":9001"
 echo '启动完成'
 
