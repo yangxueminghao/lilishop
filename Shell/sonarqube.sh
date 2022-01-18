16a28eaedaf093f3d941040d41425e8587a21fb8
Your Name <you@example.com>
2022-01-18 17:37:05 +0800

    修改部署組件

--- Shell/sonarqube.sh
+++ Shell/sonarqube.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
 echo '开始启动sonarqube'
-curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-sonarqube/master/docker-compose.yml > docker-compose.yml
-docker-compose up -d
+#curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-sonarqube/master/docker-compose.yml > docker-compose.yml
+#docker-compose up -d
+docker run -d --name sonarqube --privileged=true     -p 9000:9000  -v ${PWD}/logs:/opt/sonarqube/logs   sonarqube:9.1.0-community
+#admin/admin
 echo '启动完成'
 
